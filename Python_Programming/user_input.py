c8ba29608ebaddb26c6bc4630be949a1f7c27581
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-03-02 18:52:05 +0300

    A Python script

---
+++ Python_Programming/user_input.py
@@ -0,0 +1,5 @@
+name = input("Please enter your name: ")
+age = input("Please enter your age in years: ")
+location = input("Please enter your location: ")
+
+print("Hello {}. Your age is {} years, and your location is {}.".format(name, age, location))
