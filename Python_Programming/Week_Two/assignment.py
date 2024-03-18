2091aace1c0180a3bd1cf0508ef02ec95d8c0a01
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2024-03-18 03:18:09 +0300

    Assignment submission

---
+++ Python_Programming/Week_Two/assignment.py
@@ -0,0 +1,21 @@
+#Task 1:
+my_list = []
+
+#Task 2:
+my_list.extend([10, 20, 30, 40])
+
+#Task 3:
+my_list.insert(1, 15)
+
+#Task 4:
+my_list.extend([50, 60, 70])
+
+#Task 5:
+del my_list[-1]
+
+#Task 6:
+my_list.sort()
+
+#Task 7:
+x = my_list.index(30)
+print(f"The index of the value of 30 in the list is my_list[{x}]")
